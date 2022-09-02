76240c3600445370c61dc579f52172fc1e528f11
Alexandre Cumbane <alex_cumbane@hotmail.com>
2022-09-02 23:58:35 +0200

    ad admin url

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('snippets.urls')),
+    path('', include('snippets.url')),
+    path('admin/', admin.site.urls),
+
 ]
